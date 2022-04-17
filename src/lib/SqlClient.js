808672a01bc40cc558c4421e2bb4592acf1f0ce2
Wilmer Rodriguez <wilmer.rodriguez.czubaty@ciisa.cl>
2022-04-17 19:14:20 -0400

    pequeñas modificaciones en la vista home

--- src/lib/SqlClient.js
+++ src/lib/SqlClient.js
@@ -2,13 +2,16 @@ const mysql = require('mysql2/promise')
 
 class SqlClient {
   constructor () {
+  // Creamos el pool de conexión y le pasamos el url de mi variable de entorno
     this.pool = mysql.createPool({ uri: process.env.DATABASE_URL })
     this.query = this.query.bind(this)
   }
 
+  // Metodo para crear querys
   async query (sql, params) {
     const connection = await this.pool.getConnection()
     const response = await connection.execute(sql, params)
+    // Se cierra la conexión
     connection.release()
     return response
   }
